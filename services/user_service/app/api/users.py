345b5c18e3fe9730ae918234df14d8db9f6181d0
ASMartirosyan <ASMartirosyan@mai.education>
2025-10-22 14:16:10 +0300

    Did CRUD for users

--- services/user_service/app/api/users.py
+++ services/user_service/app/api/users.py
@@ -1,25 +1,38 @@
-from fastapi import APIRouter, HTTPException, status, Depends
-from app.schemas.user import NewUser, GetUser
+from typing import List, Optional
+
+from fastapi import APIRouter, Depends
+from app.schemas.user import NewUser, GetUser, UpdateName
 from app.services.user_service import UserService
 from app.storage.postgresql.connection import get_session, SessionFactory
 
 router = APIRouter(prefix="/users")
 
 
-@router.get("/", response_model=GetUser | dict)
-async def get_user(data: int, session: SessionFactory = Depends(get_session)):
-    try:
-        user = await UserService.get_user_by_id(data, session)
-        return GetUser.model_validate(user)
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=str(e))
+@router.get("/{user_id}", response_model=GetUser | dict)
+async def get_user(user_id: int, session: SessionFactory = Depends(get_session)):
+    user = await UserService.get_user_by_id(user_id, session)
+    return GetUser.model_validate(user)
 
 
-@router.post("/", response_model=NewUser | dict)
+@router.get("/", response_model=List[GetUser])
+async def get_all_users(session: SessionFactory = Depends(get_session)):
+    users = await UserService.get_all_users(session)
+    return users
+
+
+@router.post("/", response_model=GetUser)
 async def add_user(data: NewUser, session: SessionFactory = Depends(get_session)):
-    try:
-        user_id = await UserService.create_new_user(data, session)
-        return {"message": "ok", "user_id": user_id}
-    except ValueError as e:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+    user = await UserService.create_new_user(data, session)
+    return user
 
+@router.delete("/{iser_id}", response_model=GetUser)
+async def delete_user(user_id: int, session: SessionFactory = Depends(get_session)):
+    user = await UserService.delete_user(user_id=user_id, session=session)
+    return user
+
+@router.put("/{iser_id}", response_model=GetUser)
+async def update_user_name(data: UpdateName, session: SessionFactory = Depends(get_session)):
+    #доделать схему
+    user = await UserService.update_user_name(user_id=data.id, session=session)
+    return user
+#доделать update email
--- services/user_service/app/storage/postgresql/repositories/user_repository.py
+++ services/user_service/app/storage/postgresql/repositories/user_repository.py
@@ -1,14 +1,16 @@
+from typing import List, Optional
+
 from app.storage.postgresql.models.user_model import UserOrm
 from app.schemas.user import NewUser
 
-from sqlalchemy import select
+from sqlalchemy import select, Row
 from sqlalchemy.exc import IntegrityError, NoResultFound
 from sqlalchemy.ext.asyncio import AsyncSession
 
 class UserReposetory:
 
     @staticmethod
-    async def create_new_user(data: NewUser, session: AsyncSession) -> int:
+    async def create_new_user(data: NewUser, session: AsyncSession) -> UserOrm:
         try:
             new_user = UserOrm(
                 first_name=data.first_name,
@@ -16,28 +18,80 @@ class UserReposetory:
                 email=data.email,
             )
             session.add(new_user)
-            await session.flush()
+            await session.flush() #занести изм в бд предварительно и получить id
             await session.commit()
-            return new_user.id
+            return new_user
         except IntegrityError:
-            raise ValueError("Пользователь с таким email уже существует")
+            raise ValueError(f"Нельзя создать пользователя! Пользователь с email={data.email} уже существует")
 
     @staticmethod
-    async def get_user_by_email(user_email: str, session: AsyncSession):
+    async def delete_user_by_id(user_id: int, session: AsyncSession) -> UserOrm:
+        try:
+            delete_user = await UserReposetory.get_user_by_id(user_id=user_id, session=session)
+            await session.delete(delete_user)
+            await session.commit()
+            return delete_user
+
+        except ValueError:
+            raise ValueError(f"Невозможно удалить данные! Пользователь с id={user_id} не найден")
+
+    @staticmethod
+    async def update_user_name(user_id: int, session: AsyncSession, first_name: Optional[str] = None, last_name: Optional[str] = None) -> UserOrm:
+        try:
+            update_user = await UserReposetory.get_user_by_id(user_id=user_id, session=session)
+
+            if first_name is not None:
+                update_user.first_name = first_name
+            if last_name is not None:
+                update_user.last_name = last_name
+
+            await session.commit()
+            await session.refresh(update_user)
+            return update_user
+
+        except ValueError:
+            raise ValueError(f"Невозможно обновить данные! Пользователь с id={user_id} не найден")
+
+    @staticmethod
+    async def update_user_email(user_id: int, new_email: str, session: AsyncSession) -> UserOrm:
+        try:
+            update_user = await UserReposetory.get_user_by_id(user_id=user_id, session=session)
+
+            try:
+                user_with_new_email = await UserReposetory.get_user_by_email(user_email=new_email, session=session)
+                raise ValueError(f"Пользователь с email={new_email} уже существует")
+            except ValueError:
+                update_user.email = new_email
+                await session.commit()
+                await session.refresh(update_user)
+                return update_user
+
+        except ValueError as e:
+            raise ValueError(f"Невозможно обновить email! {e}")
+
+    @staticmethod
+    async def get_all_users(session: AsyncSession) -> List[UserOrm]:
+        query = select(UserOrm)
+        result = await session.execute(query)
+        users = result.scalars().all()
+        return users
+
+    @staticmethod
+    async def get_user_by_email(user_email: str, session: AsyncSession) -> UserOrm:
         query = select(UserOrm).where(UserOrm.email == user_email)
         try:
             result = await session.execute(query)
             user = result.scalar_one()
             return user
         except NoResultFound:
-            raise ValueError(f"Пользователь с id={user_email} не найден")
+            raise ValueError(f"Пользователь с email={user_email} не найден")
 
     @staticmethod
-    async def get_user_by_id(user_id: int, session: AsyncSession):
+    async def get_user_by_id(user_id: int, session: AsyncSession) -> UserOrm:
         query = select(UserOrm).where(UserOrm.id == user_id)
         try:
             result = await session.execute(query)
-            user = result.scalar_one()  #выбросит NoResultFound если нет
+            user = result.scalar_one() #выбросит NoResultFound если нет
             return user
         except NoResultFound:
             raise ValueError(f"Пользователь с id={user_id} не найден")
